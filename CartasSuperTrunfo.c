45fa15d2d929171cc7261392b2996ec1c4f11256
brunovg26 <bruno409vinicius@gmail.com>
2025-10-23 18:28:52 +0000

    criando o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,35 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    char estado;
+   
+    char estado [1];
     char codigo[10];
     char nome[20];
     int populacao;
     float km;  
     float PIB;  
     int pontos_turisticos;
-    int valor;
+    
+ printf("Carta 1:\n");
+    
 
-    printf("Estado: ");
+    printf("Digite um Estado: ");
     scanf(" %c", &estado);
 
-    printf("Código: ");
-    scanf("%s", codigo);
+    printf("Insira o Código: ");
+    scanf("%s", codigo );
 
-    printf("Nome da cidade: ");
+    printf("Digite o Nome da cidade: ");
     scanf("%s", nome);
 
-    printf("População: ");
-    scanf("%d", &populacao);   
+    printf("Qual é a População: ");
+    scanf("%d ", &populacao);   
 
     printf("KM: ");
     scanf("%f", &km);
     
-    printf("PIB ");
+    printf("PIB: ");
     scanf("%f", &PIB);
 
     printf("Numero de Pontos turisticos: ");
     scanf("%d", &pontos_turisticos);
+
+
+ printf("\nCarta Super Trunfo - Cidade\n");
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Nome da cidade: %s\n", nome);
+    printf("População: %d\n", populacao);
+    printf("KM: %.2f\n", km);
+    printf("PIB: %.2f\n", PIB);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos);
 
     return 0;
 }
